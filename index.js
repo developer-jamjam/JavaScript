7bc399c1137987aa0f4d5ce8eedc872347c127ba
404_jamjam <jhy1025a@gmail.com>
2023-11-30 18:00:01 +0900

    2023.11.30
    자바스크립트 마스터리 과정 Intro

---
+++ index.js
@@ -0,0 +1,3 @@
+console.log("Hello JavaScript World!vv");
+// node 파일명 해서 node로 확인 가능!
+// nodemon 설치 하여 바로바로 터미널에 반영 되도록 설정 (중지 시 ctrl + c)
---
+++ introduceJS.js
@@ -0,0 +1,23 @@
+/*
+    JavaScript 
+    브라우저 자체에 내장되어 있는 자바스크립트 엔진 필요!
+    자바스크립트 엔진이 동작하는 시간 => 런타임 시! 
+    코드를 한줄 한줄 씩 읽어서 실행 => 인터프리터
+    인터프린터 
+    - 바로실행 후 런타임시 코드를 한줄씩 번역해서 실행
+
+    컴파일러
+    - 실행하기전 모든 코드를 컴파일러를 통해서 컴파일링 하여 컴퓨터가 이해할 수 있는 언어로 변환
+    
+    자바스크립트 엔진
+        IE => Chakra / edge => V8
+        Chrome => V8
+        Safari => JavaScript Core
+        FireFox => spiderMonkey
+        NodeJS => V8
+    ECMAScript : 자바스크립트 문법의 규격사항, 표준사항을 명시 
+    ECMAScript 버전 별 히스토리 : https://en.wikipedia.org/wiki/ECMAScript
+
+    JavaScript 
+    => 프로그래밍언어 (개발자가 정해진 문법으로 특정한 로직을 수행하도록 프로그래밍함)
+*/
